cf9ec5a99ddf7025e46ca25caa1e1c204535f69f
“Your <gabrielbromo@outlook.com>
2022-11-16 08:30:51 -0300

    first commit

---
+++ lib/src/auth/sign_in_screen.dart
@@ -0,0 +1,32 @@
+ 
+
+ 
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) { 
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          //row - col (grid)
+          Expanded(child: Container(color:Colors.red)),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration( 
+                color:Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(45), 
+                ),
+              ), 
+            ),
+           
+          ) 
+        ],
+      )
+    );
+  }
+}
